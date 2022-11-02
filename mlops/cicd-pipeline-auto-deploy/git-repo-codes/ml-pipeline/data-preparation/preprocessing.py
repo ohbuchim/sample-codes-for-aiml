94a405f6a9b0327dddc61f08e4aa2dc877950c03
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-02 12:45:44 +0000

    Adding tarfile member sanitization to extractall()

--- mlops/cicd-pipeline-auto-deploy/git-repo-codes/ml-pipeline/data-preparation/preprocessing.py
+++ mlops/cicd-pipeline-auto-deploy/git-repo-codes/ml-pipeline/data-preparation/preprocessing.py
@@ -21,7 +21,26 @@ if __name__ == "__main__":
     filename = os.path.join(args.input_dir, 'mnist_png.tgz')
 
     with tarfile.open(filename, 'r:gz') as t:
-        t.extractall(path='mnist')
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(t, path="mnist")
 
     prepared_data_path = args.output_dir
     os.makedirs(prepared_data_path, exist_ok=True)
